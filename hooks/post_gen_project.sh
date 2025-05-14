7ff8d9b1573ae5701c53f2115bc8137162acb7b3
Emil Sadek <esadek@users.noreply.github.com>
2025-05-14 15:38:52 +0200

    Remove: rust-just

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 uv add dbt-core dbt-{{ cookiecutter.adapter }}
-uv add --dev sqlfluff sqlfluff-templater-dbt yamllint ruff rust-just
+uv add --dev sqlfluff sqlfluff-templater-dbt yamllint ruff
